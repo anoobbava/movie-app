96cd891c55960b7a6186ef4d8d82495d2a38c3d0
Anoob Bava <anoob.bava@gmail.com>
2019-02-16 23:24:29 +0530

    added vue-router to the app

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import { store } from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  store
+  store,
+  router
 }).$mount('#app')
