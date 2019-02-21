52258be8f5897a0feb2dc9f079143f3e7f1c8cdb
Anoob Bava <anoob.bava@gmail.com>
2019-02-21 12:58:47 +0530

    implemented the latestMovie Component

--- src/components/LatestMovie.vue
+++ src/components/LatestMovie.vue
@@ -1,22 +1,37 @@
 <template>
-  <v-container row wrap>
-    <v-layout>
-      <v-flex xs12 sm6 offset-sm3>
-        <v-card>
+
+  <v-container v-if="loading">
+    <div class="text-xs-center">
+      <v-progress-circular
+        indeterminate
+        :size="150"
+        :width="8"
+        color="green">
+      </v-progress-circular>
+    </div>
+  </v-container>
+
+  <v-container v-else>
+    <v-layout wrap>
+      <v-flex xs4 v-for="(item, index) in wholeResponse"
+              :key="index" mb-2>
+        <v-card mr-2>
           <v-img
-            :src="this.wholeResponse.Poster"
-            aspect-ratio="2.75"
+            :src="item.Poster"
+            aspect-ratio="1"
           ></v-img>
 
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">{{this.wholeResponse.Title}}</h3>
-              <div>{{this.wholeResponse.Plot}}</div>
+              <h2>{{item.Title}}</h2>
+              <div>Year: {{item.Year}}</div>
+              <div>Type: {{item.Type}}</div>
+              <div>IMDB-id: {{item.imdbID}}</div>
             </div>
           </v-card-title>
           <v-card-actions>
-            <v-btn flat color="orange">Share</v-btn>
-            <v-btn flat color="orange">Explore</v-btn>
+            <v-btn round color="green">View</v-btn>
+            <v-btn round color="green">Visit site</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -29,18 +44,25 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      wholeResponse: []
+      wholeResponse: [],
+      loading: true
     }
   },
-
   mounted () {
     axios
-      .get('http://www.omdbapi.com/?t=titanic&apikey=b76b385c&Content-Type=application/json')
-      .then(response => (this.wholeResponse = response.data))
+      .get('http://www.omdbapi.com/?s=mummy&apikey=b76b385c&page=1&type=movie&Content-Type=application/json')
+      .then(response => {
+        this.wholeResponse = response.data.Search
+        this.loading = false
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+  .v-progress-circular
+    margin: 1rem
 </style>
